0f94cea4ef4ad11a9c3a94a1ddb7eb5d60c0472d
ritterwu <ritterwu@tencent.com>
2022-10-18 10:49:49 +0800

    feat: windows build MD(d) vendor tools

--- win.msvc.cmake
+++ win.msvc.cmake
@@ -1,7 +1,8 @@
 cmake_minimum_required(VERSION 3.15)
 
-# use MT(d), if want to build MD(d) comment this command
-set(CMAKE_MSVC_RUNTIME_LIBRARY "MultiThreaded$<$<CONFIG:Debug>:Debug>")
+# use MT(d) or MD(d)
+# if you want to build MD(d), delete the last "DLL" word
+set(CMAKE_MSVC_RUNTIME_LIBRARY "MultiThreaded$<$<CONFIG:Debug>:Debug>DLL")
 # multi compile
 add_compile_options(/MP)
 # embed the debug info within lib
