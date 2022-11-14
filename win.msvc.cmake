4a353e04c660b09eaf409d843af38d907601ff3e
ritterwu <ritterwu@tencent.com>
2022-11-14 14:56:21 +0800

    chore: tidy code

--- win.msvc.cmake
+++ win.msvc.cmake
@@ -1,16 +1,14 @@
 cmake_minimum_required(VERSION 3.15)
 
 if (MSVC)
+    message("MSVC build")
     # use MT(d) or MD(d)
     # if you want to build MD(d), delete the last "DLL" word
-    message("MSVC build")
     set(CMAKE_MSVC_RUNTIME_LIBRARY "MultiThreaded$<$<CONFIG:Debug>:Debug>DLL")
+    
     # MP: # multi-processor compilation
     # Z7: pdb Z7 format
-    set(MSVC_BUILD_FLAGS /MP /Z7)
-    add_compile_options("$<$<COMPILE_LANGUAGE:C>:${MSVC_BUILD_FLAGS}>")
-    add_compile_options("$<$<COMPILE_LANGUAGE:CXX>:${MSVC_BUILD_FLAGS}>")
+    set(MSVC_BUILD_FLAGS "/MP /Z7")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${MSVC_BUILD_FLAGS}")
+    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${MSVC_BUILD_FLAGS}")
 endif ()
-
-# open arch:avx
-add_compile_options(/arch:AVX)
