5192001eb4789b114f381e035c840af9bfeb69a6
domrjchen <dom@idom.me>
2023-11-12 22:40:40 +0800

    Fix vendor link error after running cmake clean.

--- vendor.cmake
+++ vendor.cmake
@@ -130,7 +130,7 @@ function(add_vendor_target targetName)
     add_custom_command(OUTPUT ${VENDOR_OUTPUT_NAME}
             COMMAND node ${VENDOR_TOOLS_DIR}/vendor-build ${staticVendors} ${sharedVendors} -p ${PLATFORM} -v ${VENDOR_DEBUG_FLAG} -o ${VENDOR_OUTPUT_DIR}
             WORKING_DIRECTORY ${CONFIG_DIR}
-            BYPRODUCTS ${VENDOR_OUTPUT_LIB} ${VENDOR_SHARED_LIBRARIES}
+            BYPRODUCTS ${VENDOR_OUTPUT_LIB} ${VENDOR_SHARED_LIBRARIES} ${VENDOR_OUTPUT_DIR}/.vendor.sha1
             VERBATIM USES_TERMINAL)
     # set the output variables:
     set(${targetName}_VENDOR_TARGET ${VENDOR_OUTPUT_NAME} PARENT_SCOPE)
